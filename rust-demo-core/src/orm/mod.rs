0288790705892604280992aa6720b0d1b746041d
wwh <wuwh@ligeit.com>
2022-05-19 18:02:11 +0800

    global sigle const instance

--- rust-demo-core/src/orm/mod.rs
+++ rust-demo-core/src/orm/mod.rs
@@ -3,6 +3,7 @@ pub mod user;
 
 use crate::util::db_utils;
 use sea_orm::DatabaseConnection;
+use tokio::sync::OnceCell;
 
 // use async_once::AsyncOnce;
 // use lazy_static::lazy_static;
@@ -22,4 +23,15 @@ pub async fn init_app_conn() {
             APP_CONN = Some(Box::leak(c));
         }
     }
+}
+
+// 另一种全局单例变量创建示例
+pub static CONN: OnceCell<DatabaseConnection> = OnceCell::const_new();
+
+async fn init_conn() -> DatabaseConnection {
+    db_utils::create_connection().await.unwrap()
+}
+
+pub async fn get_conn() -> &'static DatabaseConnection {
+    CONN.get_or_init(init_conn).await
 }
